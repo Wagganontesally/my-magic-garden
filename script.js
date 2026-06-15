e62e3e843225480a449061cfd3d5c512a0ec5fb0
Rolyyy <136182445+Wagganontesally@users.noreply.github.com>
2026-06-15 12:26:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
+
+.message-box {
+    position: fixed;
+    top: 30px;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 9999;
+    text-align: center;
+}
+
+.message-box a {
+    color: white;
+    text-decoration: none;
+    font-size: 22px;
+    font-weight: bold;
+    background: rgba(0,0,0,0.4);
+    padding: 15px 25px;
+    border-radius: 10px;
+}
+
+.message-box a:hover {
+    text-decoration: underline;
+}
